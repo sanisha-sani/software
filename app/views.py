6a5d38d2a3e02e1022560dae2428e2b43eebda78
Maria Susan Abraham <85724556+Mariasusanabraham@users.noreply.github.com>
2022-03-14 15:13:22 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
-# def index(request):
-#     return render(request,'index.html')
+Create your views here.
+
+#***********************developer***********************************************
 def developer_Dashboard(request):
     return render(request,'developer\developer_Dashboard.html')
