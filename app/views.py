b50dfa97c91de3991ad364d8c1183c246a538540
Emil-20 <emilvarghese2000@gmail.com~>
2022-03-14 15:06:16 +0530

    Developer

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+# def index(request):
+#     return render(request,'index.html')
+def developer_Dashboard(request):
+    return render(request,'developer\developer_Dashboard.html')
