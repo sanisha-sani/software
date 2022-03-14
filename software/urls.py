4156e99382b6455a8c6de1aea74edada30f94cb8
kripamariamshaji <kripamariamshaji@gmail.com>
2022-03-14 18:12:37 -0400

    manager_commit1

--- software/urls.py
+++ software/urls.py
@@ -1,9 +1,16 @@
-from django import views
 from django.contrib import admin
 from django.urls import path
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index', views.index, name='index'),
+
+
+    
+
+    #***************************** Manager ******************************
+
+    path('manager_Dashboard',views.manager_Dashboard),
     
 ]
