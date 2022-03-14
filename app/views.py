547bc17d440a5d71f70645d698660b2927866091
kripamariamshaji <kripamariamshaji@gmail.com>
2022-03-14 16:35:26 -0400

    manager1

--- app/views.py
+++ app/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
-Create your views here.
+# Create your views here.
 
 #***********************developer***********************************************
 def developer_Dashboard(request):
     return render(request,'developer\developer_Dashboard.html')
+
+
+
+#******************  Manager ******************************************
+
+def manager_Dashboard(request):
+    return render(request,'manager\manager_Dashboard.html')
+
